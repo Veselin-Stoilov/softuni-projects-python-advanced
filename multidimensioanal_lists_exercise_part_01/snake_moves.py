26489d73e5959348d29b6b6c5788f10fd5174d5e
veselin-stoilov <vesso100@gmail.com>
2021-10-07 23:29:57 +0200

    snake moves added

---
+++ multidimensioanal_lists_exercise_part_01/snake_moves.py
@@ -0,0 +1,35 @@
+def snake_matrix_generator(string):
+    """
+    this function generates a matrix from a string
+    simulating a snake move
+    """
+    matrix = []
+
+    string_index = 0
+
+    for r in range(n):
+        column = ''
+
+        for c in range(m):
+            column += string[string_index]
+            string_index = (string_index + 1) % len(string)
+
+        if r % 2 != 0:
+            column = column[::-1]
+
+        matrix.append(column)
+
+    return matrix
+
+
+n, m = (int(x) for x in input().split())
+word = input()
+
+snake_matrix = snake_matrix_generator(word)
+
+for row in snake_matrix:
+    print(row)
+
+
+
+
