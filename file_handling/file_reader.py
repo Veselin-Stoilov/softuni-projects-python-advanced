d1676fb6f281ecbf2a80e5791d036a49152d7772
veselin-stoilov <vesso100@gmail.com>
2021-10-09 17:00:13 +0200

    numbers sum added

--- file_handling/file_reader.py
+++ file_handling/file_reader.py
@@ -2,5 +2,11 @@ file_name = r'text_documents\numbers.txt'
 
 file = open(file_name, 'r')
 
-print(file.read())
+num_sum = 0
 
+numbers = [int(x) for x in file.read() if x.isdigit()]
+
+for num in numbers:
+    num_sum += int(num)
+
+print(num_sum)
